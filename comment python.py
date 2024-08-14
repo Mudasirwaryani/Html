a00992f20b9647cc65a6a4f042cf9e4f8f1dc4b6
Mudasir waryani <mudasirwaryani@gmail.com>
2024-08-14 10:57:22 +0500

    Comment python.py

---
+++ comment python.py
@@ -0,0 +1,8 @@
+#learn how to write in next line
+#for example
+# 1. my name is ramesh.
+# 2. i am a doctor.
+# 3. i am 30 years old.
+# for this we use \n
+
+print("my name is mudasir \ni iam a doctor \ni iam 30 years old")
